c2386a3e3645b06c531cd17c167feeeaaca77072
riewzzz <riew2812@gmail.com>
2021-02-03 11:00:04 +0700

    adding a new file

---
+++ project.py
@@ -0,0 +1,59 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="game"
+)
+
+mycursor = mydb.cursor()
+
+def createTable():
+    mycursor.execute("create table if not exists more_customers (id int auto_increment primary key, name varchar(255), address varchar(255))")
+
+createTable()
+
+def existsCustomers(name, address):
+    query = "select count(*) from more_customers where name='{}'".format(name)
+    print(query)
+    mycursor.execute(query)
+    
+    myresult = mycursor.fetchall()
+    
+    
+    print("myresluts=")
+    print(myresult)
+    print("myresult[0]=")
+    print(myresult[0])
+    print("myresult[0][0]")
+    
+    
+    count = myresult[0][0]
+    print(count)
+    
+    if count == 0:
+        
+        sql = "insert into more_customers (name, address) values (%s, %s)"
+        val = (name, address)
+        mycursor.execute(sql, val)
+        
+        mydb.commit()
+        
+        print(mycursor.rowcount, "records inserted.")
+    else:
+        print("Duplicate")
+        
+    
+answer = ''
+      
+while answer != "none":
+
+    answer = input("Please insert name of customr: ")
+    
+    if answer !=  "none":
+        name = answer
+    
+        address = input("Please insert address: ")
+
+        existsCustomers(name, address)
