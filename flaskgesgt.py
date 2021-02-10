1588cbd51a90d085b9cad25d06dd10d57e046b73
riewzzz <78402230+riewzzz@users.noreply.github.com>
2021-02-10 09:12:35 +0700

    Create flaskgesgt.py
    scscsc

---
+++ flaskgesgt.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import random
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="game"
+)
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    myhtml = ""
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * from customers")
+    for i in mycursor:
+        myhtml = myhtml + str(i)
+    return myhtml
+
+
+if __name__=='__main__':
+    app.run(host = '0.0.0.0', port=80)
